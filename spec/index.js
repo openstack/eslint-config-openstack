234f228f7426b8df90907f7e123b9eb0aad238ec
Michael Krotscheck <krotscheck@gmail.com>
2015-08-28 09:43:57 -0700

    Add test to alert for rule replacements.
    Eslint carries forward a replacement list that indicates when a
    rule is about to be renamed/deprecated. This unit test checks our
    rules against that list, and will fail if it detects a pending
    replacement.
    
    Change-Id: If1a20c2f6dd198f8ee9e38625977885e29f05303

--- spec/index.js
+++ spec/index.js
@@ -20,6 +20,18 @@ describe("Unit: eslint-config-openstack", function() {
     expect(config.env).toBeFalsy();
   });
 
+  it("should alert on rule replacements.", function() {
+    var eslintReplacements = require('eslint/conf/replacements.json');
+    var rules = require('../index').rules;
+
+    /*eslint-disable guard-for-in */
+    for (var ruleName in eslintReplacements) {
+      expect(rules.hasOwnProperty(ruleName))
+        .toBeFalsy("Rule " + ruleName + " has been replaced:", eslintReplacements[ruleName]);
+    }
+    /*eslint-enable guard-for-in */
+  });
+
   it("should only have opinions on rules that exist (no zombies).", function() {
     var eslintRules = require('eslint/conf/eslint.json').rules;
     var openstackRules = require('../index').rules;
