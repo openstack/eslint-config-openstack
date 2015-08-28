bf3239b331e7a84d03b29897226e01ecc68a8e97
Michael Krotscheck <krotscheck@gmail.com>
2015-08-28 09:42:37 -0700

    Add unit test for zombie rules.
    This unit test goes through all the active rules in the current
    installed version of eslint, and errors on every one which
    we declare that is no longer in the upstream package. This will
    prevent us from having to carry forward out-of-date configuration.
    
    Change-Id: I68a6ad63f4f7ac32a9de6ae8859ffa791a03afe6

--- spec/index.js
+++ spec/index.js
@@ -1,23 +1,34 @@
-describe("Unit: eslint-config-openstack", function () {
+describe("Unit: eslint-config-openstack", function() {
 
-  it("should set espree as the default parser.", function () {
-
+  it("should set espree as the default parser.", function() {
     var config = require('../index');
     expect(config.parser).toEqual('espree');
   });
 
-  it("should disable all ecma6 features.", function () {
+  it("should disable all ecma6 features.", function() {
     var config = require('../index');
 
     var keys = Object.keys(config.ecmaFeatures);
 
-    keys.forEach(function (key) {
+    keys.forEach(function(key) {
       expect(config.ecmaFeatures[key]).toBeFalsy();
     });
   });
 
-  it("should disable all environments.", function () {
+  it("should disable all environments.", function() {
     var config = require('../index');
     expect(config.env).toBeFalsy();
+  });
+
+  it("should only have opinions on rules that exist (no zombies).", function() {
+    var eslintRules = require('eslint/conf/eslint.json').rules;
+    var openstackRules = require('../index').rules;
+
+    /*eslint-disable guard-for-in */
+    for (var ruleName in openstackRules) {
+      expect(eslintRules.hasOwnProperty(ruleName))
+        .toBeTruthy("Rule " + ruleName + " is a zombie.");
+    }
+    /*eslint-enable guard-for-in */
   });
 });
